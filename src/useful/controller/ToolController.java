a70c21505d8c8f846b9bf82f478f80a81435e976
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-10-09 08:33:59 -0600

    Added a practice list and experiemented with add, get, remove.

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -22,18 +22,37 @@ public class ToolController
 		
 		donutList.add(temp);
 		fillTheList();
-		showTheList();
+//		showTheList();
+		changeTheList();
 	}
 	
 	private void showTheList()
 	{
-		
+		String favorite = "Rasberry filled";
 		for (int index = 0; index < donutList.size(); index += 1)
 		{
+			String currentFlavor = donutList.get(index).getFlavor();
+			
+			Donut currentDonut = donutList.get(index);
+			String flavor = currentDonut.getFlavor();
+			
+			if (currentFlavor.equals(favorite))
+			{
+				for (int woop = 0; woop < 5; woop +=1)
+				{
+					display.displayText("The best flavor ever.");
+				}
+			}
+			for (int currentLetterIndex = 0; currentLetterIndex < currentFlavor.length(); currentLetterIndex += 1)
+			{
+				display.displayText(currentFlavor.substring(currentLetterIndex, currentLetterIndex + 1));
+			}
 			display.displayText(donutList.get(index).toString());
 		}
 			
+		
 	}
+	
 	
 	private void fillTheList()
 	{
@@ -47,8 +66,24 @@ public class ToolController
 		donutList.add(jellyFilled);
 		donutList.add(plainBar);
 		donutList.add(bearclaw);
-		donutList.add(krispyKreme);
-		
-			
+		donutList.add(krispyKreme);		
 	}
+
+	
+	private void changeTheList()
+	{
+		display.displayText("The list is this big: " + donutList.size());
+		Donut removed = donutList.remove(0);
+		display.displayText(removed.getFlavor() + " was removed from the list");
+		display.displayText("Now it is this big: " + donutList.size());
+		donutList.add(removed);
+		
+		display.displayText("The list still contains: " + donutList.size() + " items.");
+		removed = donutList.set(3, new Donut());
+		display.displayText("The dout with flavor " + removed.getFlavor() + " has been removed.");
+		
+	}
+	
+	
+	
 }
